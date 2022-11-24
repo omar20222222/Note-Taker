8be632bcd9629409285e0ccf2ee26befc85cbcae
omar20222222 <108297683+omar20222222@users.noreply.github.com>
2022-11-24 14:32:53 +0000

    updated index.html

--- server.js
+++ server.js
@@ -4,19 +4,15 @@ const fs = require("fs");
 const uuid = require("uuid");
 console.log(uuid.v4());
 
-// const api = require("./routes/index.js");
-
 const PORT = 3001;
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-// app.use("/api", api);
 
 app.use(express.static("public"));
 
-//HTML routes
 app.get("/", (req, res) =>
   res.sendFile(path.join(__dirname, "/public/index.html"))
 );
@@ -25,7 +21,6 @@ app.get("/notes", (req, res) =>
   res.sendFile(path.join(__dirname, "/public/notes.html"))
 );
 
-// api routes
 app.get("/api/notes", (req, res) => {
   fs.readFile("./db/db.json", "utf8", (err, data) => {
     if (err) {
